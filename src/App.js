2c8c3c0f1707f32cb9dd4c9d2324d78d39b341ea
Gayath1 <chandulagayan@gmail.com>
2020-09-24 00:15:01 +0530

    Milk.lk Store ui

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './index.css'
+import data from "./data";
+
 
 function App() {
+
+    const openmenu =() =>{
+        document.querySelector(".sidebar").classList.add("open");
+    }
+    const closemenu = () =>{
+        document.querySelector(".sidebar").classList.remove("open");
+    }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="grid-container">
+      <header className="header">
+          <div className="brand">
+              <button onClick={openmenu}>
+                  &#9776;
+              </button>
+              <a href="index.js">Milk.lk</a>
+          </div>
+          <div className="header-links">
+              <a href="singin" >sign in </a>
+
+              <a href="cart">cart</a>
+          </div>
       </header>
+      <aside className="sidebar">
+          <h3>Product Categories</h3>
+          <button className="sidebar-close-button" onClick={closemenu}>x</button>
+          <ul>
+              <li>
+                  <a href="null">Milk</a>
+              </li>
+              <li>
+                  <a href="null">Butters</a>
+              </li>
+          </ul>
+
+      </aside>
+        <main className="main">
+            <div className="content">
+                <ul className="products">
+                    {
+                        data.products.map(products =>
+                            <li>
+
+                                <div className="product">
+                                    <img className="product-img" src={products.image} alt="Product"/>
+                                    <div className="product-name">
+                                        <a href="product.html">{products.name}</a>
+                                    </div>
+                                    <div className="Product-brand">{products.brand}</div>
+                                    <div className="price">${products.price}</div>
+
+                                </div>
+
+                            </li>)
+                    }
+
+
+            </ul>
+            </div>
+        </main>
+        <footer className="footer">
+            All right reserved!
+        </footer>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,18 @@
+
+export default  {
+    products:[{
+        name:'Fresh milk-100g',
+        category:'Milk',
+        image:"./100fresh.png",
+        price:185,
+        brand:'Highland',
+    },
+        {
+            name:'Fresh milk-50g',
+            category: 'Milk',
+            image:'/100 fresh.png',
+            price:100,
+            brand:'Highland',
+        },
+    ]
+}
